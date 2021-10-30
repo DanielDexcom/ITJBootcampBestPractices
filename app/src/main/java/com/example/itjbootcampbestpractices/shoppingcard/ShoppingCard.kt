ee64f9d91b60457872f3680e165b6a4871f2d24a
DanielDexcom <dm0520@dexcom.com>
2021-10-30 08:13:36 -0700

    Added instructions to shoppincard class

--- app/src/main/java/com/example/itjbootcampbestpractices/shoppingcard/ShoppingCard.kt
+++ app/src/main/java/com/example/itjbootcampbestpractices/shoppingcard/ShoppingCard.kt
@@ -3,12 +3,14 @@ package com.example.itjbootcampbestpractices.shoppingcard
 /*
 * 1 - User should be able to add elements to shopping card
 * 2- Shopping card should provide the total price of all products
-* 3- If price is greater than $100 then no shipping cost shall be applied
-* 4- If price is lower than $100 then each product shipping cost is applied
-* 5- Tax is applied based on region
-* 6- Supported regions are (West, Midwest, Northeast, Southwest, Southeast)
-* 7- Region taxes [West = .11, Midwest = .09, Northeast = .11, Southwest = 0.08, Southeast = 0.09]
-* 8- If the user has more than $200 in products we provide a coupon of $5
+* 3- If no products in shopping card then price is 0
+* 4- If price is greater than $100 then no shipping cost shall be applied
+* 5- If price is lower than $100 then each product shipping cost is applied
+* 6- Tax is applied based on region
+* 7- Supported regions are (West, Midwest, Northeast, Southwest, Southeast)
+* 8- Region taxes [West = .11, Midwest = .09, Northeast = .11, Southwest = 0.08, Southeast = 0.09]
+* 9- If the user has more than $200 in products we provide a coupon of $5
 * */
 class ShoppingCard {
+    
 }
